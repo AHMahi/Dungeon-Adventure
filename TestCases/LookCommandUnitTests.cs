719fcc96b367b5fd29384ee6d8f0db27a10d3731
AHMahi <87108050+AHMahi@users.noreply.github.com>
2022-11-30 08:19:21 +1100

    Add files via upload
    Nunit test cases of various functionalites

---
+++ TestCases/LookCommandUnitTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using SwinAdventures;
+
+namespace IdentifiableObjectTest
+{
+    public class LookCommandUnitTests
+    {
+        [Test]
+
+        public void TestLookAtMe()
+        {
+            Player newPlayer = new Player("me", "inventory");
+            LookCommand myCommand = new LookCommand(new string[] { "look" });
+
+            Assert.AreEqual(myCommand.Execute(newPlayer, new string[] { "look", "at", "me" }), newPlayer.FullDescription);
+        }
+
+        [Test]
+
+        public void TestLookAtGem()
+        {
+            Player newPlayer = new Player("me", "inventory");
+            Item gem = new Item(new string[] { "gem" }, "Shiny Gem", "this is a gem");
+            newPlayer.Inventory.Put(gem);
+            LookCommand myCommand = new LookCommand(new string[] { "look" });
+
+            Assert.AreEqual(myCommand.Execute(newPlayer, new string[] { "look", "at", "gem" }), gem.FullDescription);
+        }
+
+        [Test]
+
+        public void TestLookAtUnk()
+        {
+            Player newPlayer = new Player("me", "inventory");
+            LookCommand myCommand = new LookCommand(new string[] { "look" });
+            Item gem = new Item(new string[] { "gem" }, "Shiny Gem", "this is a gem");
+
+            Assert.AreEqual(myCommand.Execute(newPlayer, new string[] { "look", "at", "gem" }), "I can't find the gem");
+        }
+
+        [Test]
+
+        public void TestLookAtGemInMe()
+        {
+            Player newPlayer = new Player("me", "inventory");
+
+            LookCommand myCommand = new LookCommand(new string[] { "look" });
+            Item gem = new Item(new string[] { "gem" }, "Shiny Gem", "this is a gem");
+
+            newPlayer.Inventory.Put(gem);
+
+            Assert.AreEqual(myCommand.Execute(newPlayer, new string[] { "look", "at", "gem", "in", "inventory" }), gem.FullDescription); 
+        }
+
+        [Test]
+
+        public void TestLookAtGemInBag()
+        {
+            Player newPlayer = new Player("me", "inventory");
+            Bag myBag = new Bag(new string[] { "bag" }, "a backpack", "A small backpack to store items");
+            Item gem = new Item(new string[] { "gem" }, "Shiny Gem", "this is a gem");
+            LookCommand myCommand = new LookCommand(new string[] { "look" });
+
+            myBag.Inventory.Put(gem);
+            newPlayer.Inventory.Put(myBag);
+
+            Assert.AreEqual(myCommand.Execute(newPlayer, new string[] { "look", "at", "gem", "in", "bag" }), gem.FullDescription);
+        }
+
+        [Test]
+
+        public void TestLookAtGemInNoBag()
+        {
+            Player newPlayer = new Player("me", "inventory");
+            Bag myBag = new Bag(new string[] { "bag" }, "a backpack", "A small backpack to store items");
+            Item gem = new Item(new string[] { "gem" }, "Shiny Gem", "this is a gem");
+            LookCommand myCommand = new LookCommand(new string[] { "look" });
+
+            Assert.AreEqual(myCommand.Execute(newPlayer, new string[] { "look", "at", "gem", "in", "bag" }), "I can't find the bag");
+        }
+
+        [Test]
+
+        public void TestLookAtNoGemInBag()
+        {
+            Player newPlayer = new Player("me", "inventory");
+            Bag myBag = new Bag(new string[] { "bag" }, "a backpack", "A small backpack to store items");
+            Item gem = new Item(new string[] { "gem" }, "Shiny Gem", "this is a gem");
+            LookCommand myCommand = new LookCommand(new string[] { "look" });
+
+            newPlayer.Inventory.Put(myBag);
+
+            Assert.AreEqual(myCommand.Execute(newPlayer, new string[] { "look", "at", "gem", "in", "bag" }), "Can't find gem in bag");
+        }
+
+        [Test]
+
+        public void TestInvalidLook()
+        {
+            Player newPlayer = new Player("me", "inventory");
+            LookCommand myCommand = new LookCommand(new string[] { "look" });
+
+            Assert.AreEqual(myCommand.Execute(newPlayer, new string[] { "Hello" }), "Error in look input");
+        }
+    }
+}
---
+++ TestCases/PlayerUnitTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using SwinAdventures;
+
+namespace IdentifiableObjectTest
+{
+    [TestFixture()]
+    public class PlayerUnitTests
+    {
+        [Test]
+
+        public void TestPlayerisIdentifiable()
+        {
+            Player newPlayer = new Player("me", "inventory");// since we passed in "me" and "inventory" when we made the Player object ( by passsing values via base constructor) we need to make sure
+            //only "me" and "inventory" is always passed, when we call AreYou() method
+
+            Assert.True(newPlayer.AreYou("me"));
+            Assert.True(newPlayer.AreYou("inventory"));
+        }
+
+        [Test]
+
+        public void TestPlayerLocatesItems()
+        {
+            
+            Player newPlayer = new Player("me", "inventory");
+            Item rifle = new Item(new string[] { "rifle" }, "a rifle", "This is a automatic rifle ...");
+            newPlayer.Inventory.Put(rifle);
+
+            var locate = newPlayer.Locate("rifle");
+
+            Assert.AreEqual(rifle, locate);
+        }
+
+        [Test]
+
+        public void TestPlayerLocatesItself()
+        {
+            Player newPlayer = new Player("me", "inventory");
+
+            Assert.AreEqual(newPlayer, newPlayer.Locate("me"));
+            Assert.AreEqual(newPlayer, newPlayer.Locate("inventory"));
+        }
+
+        [Test]
+
+        public void TestPlayerLocatesNothing()
+        {
+            Player newPlayer = new Player("me", "inventory");
+            Item rifle = new Item(new string[] { "rifle", "automatic rifle" }, "a rifle", "This is a automatic rifle ...");
+            Inventory inventory = new Inventory();
+            inventory.Put(rifle);
+
+            Assert.AreEqual(null, newPlayer.Locate("sheild"));
+        }
+
+        [Test]
+
+        public void TestPlayerFullDescription()
+        {
+            Player newPlayer = new Player("me", "inventory");
+            newPlayer.Inventory.Put(new Item(new string[] { "rifle" }, "a rifle", "A automatic 30 round rifle"));
+            newPlayer.Inventory.Put(new Item(new string[] { "shovel" }, "a shovel", "A shovel for digging"));
+
+            string description = "You are carrying:\n" + "\ta rifle (rifle)\n" + "\ta shovel (shovel)\n";
+
+            Assert.AreEqual(description, newPlayer.FullDescription);
+        }
+
+    }
+}
